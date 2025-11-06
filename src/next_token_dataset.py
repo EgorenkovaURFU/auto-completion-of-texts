c9480bb7029f4b05c4150669669ef16c68d267bd
Teatyana Egorenkova <tatyana-baeva@list.ru>
2025-11-06 11:20:15 +0700

    rewrite custom dataset

--- src/next_token_dataset.py
+++ src/next_token_dataset.py
@@ -1,22 +1,36 @@
 
 import torch
 from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_packed_sequence
 
 class CustomDataset(Dataset):
 
-    def __init__(self, texts, labels, tokeniser, max_len=256):
+    def __init__(self, inputs: list[list[str]], targets: list[list[int]]):
         super().__init__()
-        self.encodings = tokeniser(texts, paddings='max_lenght', truncation=True, 
-                                   max_length=max_len, return_tensors='pt')
-        self.labels = torch.tensor()
+
+        assert len(inputs) == len(targets)
+
+        self.inputs = inputs
+        self.targets = targets
+
+        # self.encodings = tokeniser(texts, paddings='max_lenght', truncation=True, 
+        #                            max_length=max_len, return_tensors='pt')
+        # self.labels = torch.tensor()
         
     def __len__(self):
-        return len(self.labels)
+        return len(self.targets)
     
     def __getitem__(self, idx):
-        return {
-            'input_ids': self.encodings['input_ids'][idx],
-            'attaention_mask': self.encodings['attantion_mask'][idx],
-            'label': self.labels[idx]
-        }
+        return torch.tensor(self.inputs[idx], dtype=torch.long), torch.tensor(self.targets[idx], dtype=torch.long)
+
     
+def collate_fn(batch, pad_id):
+
+    inps = [b[0] for b in batch]
+    tgts = [b[1] for b in batch]
+    inps_p = pad_packed_sequence(inps, batch_first=True, padding_value=pad_id)
+    tgts_p = pad_packed_sequence(tgts, batch_first=True, padding_value=pad_id)
+    mask = (tgts_p != pad_id)
+
+    return inps_p, tgts_p, mask
+
