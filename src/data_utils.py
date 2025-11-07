a16e727067b6f95a05cc7d479094bc1ddfe16c54
Teatyana Egorenkova <tatyana-baeva@list.ru>
2025-11-07 09:17:13 +0700

    add clean data

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,4 +1,5 @@
 import re
+import unicodedata
 
 
 def clean_text(text):
@@ -7,7 +8,10 @@ def clean_text(text):
     This function cleans a text: 
     lowercase, save letters and numbers, delete double space.
     """
+    if not isinstance(text, str):
+        return ""
 
+    text = unicodedata.normalize("NFKC", text)
     text = text.lower()
     text = re.sub(r"[^a-z0-9]+", " ", text)
     text = re.sub(r"\s+", " ", text)
